365866e05e39e0a941bc93839ce99b1068daee33
Edgar Montoya <128949786+edmon2@users.noreply.github.com>
2023-11-09 18:04:28 -0600

    mi chamba

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RolController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,4 +27,9 @@ Route::get('/formulario_roles', function () {
     return view('roles');
 });
 
+Route::get('/formulario_usuarios', function () {
+    return view('usuarios')->with(['nombre_rol'=>'Rol_1','id'=>1]);
+});
+
 Route::post('/insertar_roles', [RolController::class, "store"])->name('rol.store');
+Route::post('/insertar_usuarios', [UserController::class, "store"])->name('usuario.store');
