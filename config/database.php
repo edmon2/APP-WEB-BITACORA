4947ad151b9da0ebf39abe413210f5e7145b2732
Jose_LeonJL <60480200+Jose-LeonJL@users.noreply.github.com>
2023-11-09 18:45:27 -0600

    zzz

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'recepcionequiposgrupoh'),
-            'username' => env('DB_USERNAME', 'seminario_user'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'Manino10*'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
